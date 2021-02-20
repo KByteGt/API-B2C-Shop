5b61e21b93ae3926c6ef9bdcd6d8d1f3da160442
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2021-02-19 20:27:51 -0600

    adding middlewares

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const api = require('./web/api');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, DELETE, PUT, UPDATE');
+    res.setHeader('Access-Control-Allow-Header','*');
+})
+
+// All API routes
+app.use('/api', api);
 
 const PORT = process.env.PORT || 5000;
 
