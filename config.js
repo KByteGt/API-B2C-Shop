37cc4b9a6cd8a93968d40fd221f1f0de32fe0952
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2021-05-05 16:30:14 -0600

    Fixing url images

--- config.js
+++ config.js
@@ -1,30 +1,23 @@
 const multer = require('multer');
 //const { v4: uuid4} = require('uuid');
 
-if(process.env.NODE_ENV == 'production'){
-    {
-        exports.server = {
-            schema: 'https',
-            domain: 'b2c-shop-api-dot-b2c-shop-fortnite.uc.r.appspot.com',
-            ip: process.env.IP,
-            port: process.env.PORT || 8080,
-        }
-    }
-} else {
-    {
-        exports.server = {
-            schema: 'http',
-            domain: 'localhost',
-            ip: '127.0.0.1',
-            port: process.env.PORT || 2500,
-        }
-    }
+const schema = (process.env.NODE_ENV == 'production')? 'https' : 'http';
+const domain = (process.env.NODE_ENV == 'production')? 'b2c-shop-api-dot-b2c-shop-fortnite.uc.r.appspot.com' : 'localhost';
+const ip = (process.env.NODE_ENV == 'production')? process.env.IP : '127.0.0.1';
+const port = process.env.PORT || 8080;
+
+
+exports.server = {
+    schema,
+    domain,
+    ip,
+    port,
 }
 
 const type = ['image/png', 'image/jpeg', 'image/jpg'];
 const publicDir = 'public/img';
 
-exports.getDir = (process.env.NODE_ENV == 'production') ? "https://" + process.env.IP  : "http://127.0.0.1:" + process.env.PORT || 2500;
+exports.getDir = (process.env.NODE_ENV == 'production') ? "https://" + domain  : "http://127.0.0.1:" + process.env.PORT || 2500;
 
 exports.fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
